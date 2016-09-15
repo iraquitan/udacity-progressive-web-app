3183f9fefa30953f1a22c4d5f3319d1ea7bc8508
Iraquitan Cordeiro Filho <iraquitanfilho@gmail.com>
2016-09-15 09:40:53 -0300

    feat: Add more Gulp tasks and update package.json

--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,66 @@
  * Created by iraquitan on 9/12/16.
  */
 var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var del = require('del');
+var swPrecache = require('sw-precache');
 
-// Static server
 
-gulp.task('default', function () {
-    gulp.watch('./scripts/*.js').on('change', browserSync.reload);
-    gulp.watch('./index.html').on('change', browserSync.reload);
-    gulp.watch('./styles/*.css').on('change', browserSync.reload);
-    browserSync.init({
-        server: {
-            baseDir: './'
-        },
+gulp.task('sass', function () {
+    return gulp
+        .src('./styles/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('./styles/'))
+        .pipe(minifyCss({}))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./styles/'));
+});
+
+gulp.task('generate-sw', function () {
+    var swOptions = {
+        staticFileGlobs: [
+            './index.html',
+            './images/*.{png,svg,gif,jpg}',
+            './scripts/*.js',
+            './styles/*.css',
+            './node_modules/localforage/dist/localforage.min.js'
+        ],
+        stripPrefix: '.',
+        runtimeCaching: [{
+            urlPattern: /^https:\/\/publicdata-weather\.firebaseio\.com/,
+            handler: 'networkFirst',
+            options: {
+                cache: {
+                    name: 'weatherData-v3'
+                }
+            }
+        }]
+    };
+    return swPrecache.write('./service-worker.js', swOptions);
+});
+
+gulp.task('serve', ['generate-sw'], function() {
+    gulp.watch('./styles/*.scss', ['sass']);
+    browserSync({
+        notify: false,
+        logPrefix: 'weatherPWA',
+        server: ['.'],
+        // open: false,
         browser: ['google chrome', 'firefox', 'safari']
-    })
+    });
+    gulp.watch([
+        './*.html',
+        './scripts/*.js',
+        './styles/*.css',
+        '!./service-worker.js',
+        '!./gulpfile.js'
+    ], ['generate-sw'], browserSync.reload);
 });
 
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: './'
-        },
-        browser: 'google chrome'
-    });
-});
+gulp.task('default', ['serve']);
